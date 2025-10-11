03a9b8f2696b6147965025a9087bc69e2b3eef78
Matt Gardner <matt@m-gardner.info>
2025-10-11 18:26:24 -0400

    Camera Kinda Fix
    Camera Kinda Fix

--- Scenes/camera_2d.gd
+++ Scenes/camera_2d.gd
@@ -14,13 +14,13 @@ func _physics_process(delta: float) -> void:
 	# Camera movement and zoom code
 	#TODO: update to actually reflect direction player is facing when calculating offset - original script stole from car game
 	position = position.lerp(player.global_position + Vector2(-150*(player.velocity.length()/850),0).rotated(player.rotation),6*delta)
-	var zoom_level = lerp(zoom[0],4-(player.velocity.length()/100)/2,1*delta)
-	zoom = Vector2(zoom_level,zoom_level)
+	#var zoom_level = lerp(zoom[0],4-(player.velocity.length()/100)/2,1*delta)
+	#zoom = Vector2(zoom_level,zoom_level)
 
 func _process(delta: float) -> void:
 	# Camera shake forcing
-	if(Input.is_action_just_pressed("shake")):
-		apply_shake()
+	#if(Input.is_action_just_pressed("shake")):
+	#	apply_shake()
 	if shake_strength > 0:
 		shake_strength = lerpf(shake_strength, 0, shakeFade * delta)
 		offset = randomOffset()
