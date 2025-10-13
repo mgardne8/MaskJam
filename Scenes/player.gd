5484bcada0aeddda4f6d675f84f86911f25325db
Matt Gardner <matt@m-gardner.info>
2025-10-13 12:06:12 -0400

    Start of player rewrite using state charts

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,160 +3,66 @@ class_name Player
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const COYOTE_TIME_LIMIT = 0.2
-const MAX_JUMPS = 1
-const WALL_PUSHBACK = 300
-const WALL_JUMP_DISABLE_TIME = .2
+const JUMP_COYOTE_LIMIT = 0.2
+const JUMP_COUNT_MAX = 2
+const JUMP_WALL_DURATION = 0.15
+const JUMP_WALL_STRENGTH = 50
 
 var direction = 0.0
-var  coyote_timer = 0.0
-var  jump_count = 0
-enum jump_last_wall_states {NONE,LEFT,RIGHT}
-var  jump_last_wall_state = jump_last_wall_states.NONE
-var  jump_move_disable_time = 10
-
-var colour_mask = Global.Colour_States.K
-var current_colour = Global.colourDict[colour_mask]
-
-
-enum player_states {IDLE,RUN,JUMP,FALL}
-var  current_state = player_states.IDLE
+var jump_coyote_time = 0.0
+var jump_count = 0
+var jump_wall_time = 0.0
+var jump_wall_last_side = null
 
 func _ready() -> void:
 	var colour_mask = Global.Colour_States.K
+	set_collision_layer_value(1,true)
+	set_collision_mask_value(1,true)
 	set_collision_layer_value(2,true)
 	set_collision_mask_value(3,false)
 	set_collision_mask_value(4,false)
 	set_collision_mask_value(5,false)
 	$AnimatedSprite2D.material.set_shader_parameter("colour", Global.colourDict[colour_mask])
-	$AnimatedSprite2D.play("IDLE")
 
-func _physics_process(delta: float) -> void:
-	
-	jump_move_disable_time += delta
-	direction = 0.0
-	#var direction := Input.get_axis("move_left", "move_right")
-	if jump_move_disable_time >= WALL_JUMP_DISABLE_TIME or (jump_last_wall_state == jump_last_wall_states.NONE):
-		direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	elif (jump_last_wall_state == jump_last_wall_states.LEFT) and Input.is_action_pressed("move_left"):
-		print("left disabled")
-		direction = Input.get_action_strength("move_right")
-	elif (jump_last_wall_state == jump_last_wall_states.RIGHT) and Input.is_action_pressed("move_right"):
-		print("right_disabled")
-		direction = Input.get_action_strength("move_left")
-		
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if not is_on_floor():
+func die():
+	print("PLAYER DIE")
+
+# Movement Functions
+func CalcMovement(delta: float) -> void:
+	direction = Input.get_axis("move_left","move_right")
+
+	# WallGrab Gravity Modification Logic
+	if not is_on_floor() and velocity.y > 0 and (
+		($"RayCast2D-LEFT".is_colliding() and Input.is_action_pressed("move_left"))or 
+		($"RayCast2D-RIGHT".is_colliding() and Input.is_action_pressed("move_right"))
+	):
+		velocity += (get_gravity() * delta*.1)
+		print('.1 gravity')
+	else:
 		velocity += get_gravity() * delta
-		coyote_timer += delta
-	else:
-		coyote_timer = 0
-		
-	# jump.
-	if Input.is_action_just_pressed("jump"):
-		#basic jump
-		if coyote_timer < COYOTE_TIME_LIMIT and current_state != player_states.JUMP: 
-			velocity.y = JUMP_VELOCITY	
-		
-		#walljump before consuming doublejump
-		elif(current_state == player_states.JUMP or player_states.FALL)\
-		and (coyote_timer > COYOTE_TIME_LIMIT)\
-		and (
-			($"RayCast2D-LEFT".is_colliding() and (jump_last_wall_state != jump_last_wall_states.LEFT))\
-		 or ($"RayCast2D-RIGHT".is_colliding() and (jump_last_wall_state != jump_last_wall_states.RIGHT))\
-		):
-			print("WALLJUMP!")
-			print($"RayCast2D-LEFT".is_colliding())
-			print($"RayCast2D-RIGHT".is_colliding())
-			if $"RayCast2D-LEFT".is_colliding() and (jump_last_wall_state != jump_last_wall_states.LEFT):
-				print("jump off LEFT")
-				velocity.y = JUMP_VELOCITY
-				velocity.x += WALL_PUSHBACK
-				jump_last_wall_state = jump_last_wall_states.LEFT
-				jump_move_disable_time = 0
-				jump_count = 0
-			elif $"RayCast2D-RIGHT".is_colliding() and (jump_last_wall_state != jump_last_wall_states.RIGHT):
-				print("jump off RIGHT")
-				velocity.y = JUMP_VELOCITY
-				velocity.x += -WALL_PUSHBACK
-				jump_last_wall_state = jump_last_wall_states.RIGHT
-				jump_move_disable_time = 0
-				jump_count = 0
-			print('WallJumpVelocity:')
-			print(velocity)
-		#Double jump
-		elif (current_state == player_states.JUMP or player_states.FALL) and jump_count < MAX_JUMPS and coyote_timer > COYOTE_TIME_LIMIT:
-			velocity.y = JUMP_VELOCITY	
-			jump_count += 1
 	
-	if is_on_floor(): #reset jump count if on floor
-		jump_count = 0
-		jump_last_wall_state = jump_last_wall_states.NONE
-
-	#colour change controls
-	if Input.is_action_just_pressed("colour_k"):
-		colour_mask = Global.Colour_States.K
-		set_collision_layer_value(2,true)
-		set_collision_mask_value(2,true)
-		set_collision_layer_value(3,false)
-		set_collision_mask_value(3,false)
-		set_collision_layer_value(4,false)
-		set_collision_mask_value(4,false)
-		set_collision_layer_value(5,false)
-		set_collision_mask_value(5,false)
-		
-	if Input.is_action_just_pressed("colour_c"):
-		colour_mask = Global.Colour_States.C
-		set_collision_layer_value(2,false)
-		set_collision_mask_value(2,false)
-		set_collision_layer_value(3,true)
-		set_collision_mask_value(3,true)
-		set_collision_layer_value(4,false)
-		set_collision_mask_value(4,false)
-		set_collision_layer_value(5,false)
-		set_collision_mask_value(5,false)
-		
-	if Input.is_action_just_pressed("colour_y"):
-		colour_mask = Global.Colour_States.Y
-		set_collision_layer_value(2,false)
-		set_collision_mask_value(2,false)
-		set_collision_layer_value(3,false)
-		set_collision_mask_value(3,false)
-		set_collision_layer_value(4,true)
-		set_collision_mask_value(4,true)
-		set_collision_layer_value(5,false)
-		set_collision_mask_value(5,false)
-		
-	if Input.is_action_just_pressed("colour_m"):
-		colour_mask = Global.Colour_States.M
-		set_collision_layer_value(2,false)
-		set_collision_mask_value(2,false)
-		set_collision_layer_value(3,false)
-		set_collision_mask_value(3,false)
-		set_collision_layer_value(4,false)
-		set_collision_mask_value(4,false)
-		set_collision_layer_value(5,true)
-		set_collision_mask_value(5,true)
-
-	# Animation control
-	if not is_on_floor():
-		if velocity.y < 0:
-			current_state = player_states.JUMP
-		else:
-			current_state = player_states.FALL
+	if direction:
+		#velocity.x = lerp(velocity.x,direction*SPEED,3*delta)
+		velocity.x = direction*SPEED
 	else:
-		if direction:
-			current_state = player_states.RUN
-		else:
-			current_state = player_states.IDLE
-
-	if $AnimatedSprite2D.animation != player_states.keys()[current_state]:
-		$AnimatedSprite2D.play(player_states.keys()[current_state])
+		if is_on_floor():
+			#velocity.x = move_toward(velocity.x, 0, SPEED*5*delta)
+			velocity.x = 0
+			print(velocity.x)
+		else: # less horizonal friction in the air
+			velocity.x = move_toward(velocity.x, 0, SPEED*delta)
 	
+	# Calculate Cayote timer and zero out y velocity
+	if is_on_floor():
+		jump_coyote_time = 0
+		velocity.y = 0
+		jump_wall_last_side = null
+	else:
+		jump_coyote_time += delta
+		if velocity.y > 0:
+			$StateChart.send_event("Fall")
+	
+	# Calculate if sprite needs to be flipped
 	if direction > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif direction < 0:
@@ -164,23 +70,105 @@ func _physics_process(delta: float) -> void:
 	else:
 		pass
 
-	#$AnimatedSprite2D.material.set_shader_parameter("colour", Global.colourDict[colour_mask])
-	#current_colour. ( Global.colourDict[colour_mask], delta)
-	current_colour = Vector4(
-	lerp(current_colour.x,Global.colourDict[colour_mask].x,5*delta),
-		lerp(current_colour.y,Global.colourDict[colour_mask].y,5*delta),
-		lerp(current_colour.z,Global.colourDict[colour_mask].z,5*delta),
-		1)
+func CalcJump(delta: float) -> void:
+	if not jump_wall_time and Input.is_action_just_pressed("jump") and (
+		(is_on_floor() or jump_coyote_time < JUMP_COYOTE_LIMIT) or
+		(jump_count < JUMP_COUNT_MAX)
+	):
+		$StateChart.send_event("Jump")
+		velocity.y = JUMP_VELOCITY
+		if not is_on_floor() and jump_count > 0:
+			velocity.x *= 2
 
-	$AnimatedSprite2D.material.set_shader_parameter("colour", current_colour)
+func CalcWallJump(delta: float) -> void:
+	if Input.is_action_just_pressed("jump") and (
+		$"RayCast2D-LEFT".is_colliding() or $"RayCast2D-RIGHT".is_colliding()
+	):
+		$StateChart.send_event("Wall Jump")
+		velocity.y = JUMP_VELOCITY
+		jump_wall_time = 0.00001
+
+func CalcWallJumpMove(delta: float) -> void:
+	var wj_dir := 1 if jump_wall_last_side == $"RayCast2D-LEFT" else -1
+	velocity.x = move_toward(velocity.x, SPEED*wj_dir, JUMP_WALL_STRENGTH)
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+# State and Event Processing
+##Idle
+func _on_idle_state_entered() -> void:
+	jump_count = 0
+	$AnimatedSprite2D.play("IDLE")
+
+func _on_idle_state_physics_processing(delta: float) -> void:
+	CalcMovement(delta)
+	CalcJump(delta)
 	move_and_slide()
+	if direction:
+		$StateChart.send_event("Moved")
 
+##Walk
+func _on_walking_state_entered() -> void:
+	jump_count = 0
+	#TODO: Add Walk Animation
+	$AnimatedSprite2D.play("RUN")
 
+func _on_walking_state_physics_processing(delta: float) -> void:
+	CalcMovement(delta)
+	CalcJump(delta)
+	move_and_slide()
+	if not direction and velocity.x == 0:
+		$StateChart.send_event("Stopped")
 
-func die():
-	print("PLAYER DIE")
+##Jump
+func _on_jumping_state_entered() -> void:
+	jump_count += 1
+	$AnimatedSprite2D.play("JUMP")
+	print('JUMP')
+
+func _on_jumping_state_physics_processing(delta: float) -> void:
+	CalcMovement(delta)
+	CalcWallJump(delta)
+	CalcJump(delta)
+	move_and_slide()
+	if is_on_floor(): #We are no longer jumping, so switch to grounded state
+		if direction:
+			$StateChart.send_event("Landed")
+		else:
+			$StateChart.send_event("Stopped")
+
+##Fall
+func _on_falling_state_entered() -> void:
+	#TODO: Add Fall Animation
+	$AnimatedSprite2D.play("FALL")
+
+func _on_falling_state_physics_processing(delta: float) -> void:
+	CalcMovement(delta)
+	CalcWallJump(delta)
+	CalcJump(delta)
+	move_and_slide()
+	if is_on_floor(): #We are no longer jumping, so switch to grounded state
+		if direction:
+			$StateChart.send_event("Landed")
+		else:
+			$StateChart.send_event("Stopped")
+
+##Wall Jump
+func _on_wall_jumping_state_entered() -> void:
+	velocity.x = 0
+	jump_count = 0 # Reset jump count on wall jump
+	jump_wall_last_side = $"RayCast2D-LEFT" if $"RayCast2D-LEFT".is_colliding() else $"RayCast2D-RIGHT"
+	
+	#TODO: Add WallJump Animation
+	$AnimatedSprite2D.play("JUMP")
+
+func _on_wall_jumping_state_physics_processing(delta: float) -> void:
+	if jump_wall_time < JUMP_WALL_DURATION:
+		print('wall jumping')
+		jump_wall_time += delta
+		CalcMovement(delta)
+		CalcWallJumpMove(delta)
+		move_and_slide()
+	else:
+		jump_wall_time = 0.0
+		$StateChart.send_event("WallJumpEnd")
+	pass # Replace with function body.
---
+++ Scenes/player_old.gd
@@ -0,0 +1,186 @@
+extends CharacterBody2D
+#class_name Player
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const COYOTE_TIME_LIMIT = 0.2
+const MAX_JUMPS = 1
+const WALL_PUSHBACK = 300
+const WALL_JUMP_DISABLE_TIME = .2
+
+var direction = 0.0
+var  coyote_timer = 0.0
+var  jump_count = 0
+enum jump_last_wall_states {NONE,LEFT,RIGHT}
+var  jump_last_wall_state = jump_last_wall_states.NONE
+var  jump_move_disable_time = 10
+
+var colour_mask = Global.Colour_States.K
+var current_colour = Global.colourDict[colour_mask]
+
+
+enum player_states {IDLE,RUN,JUMP,FALL}
+var  current_state = player_states.IDLE
+
+func _ready() -> void:
+	var colour_mask = Global.Colour_States.K
+	set_collision_layer_value(2,true)
+	set_collision_mask_value(3,false)
+	set_collision_mask_value(4,false)
+	set_collision_mask_value(5,false)
+	$AnimatedSprite2D.material.set_shader_parameter("colour", Global.colourDict[colour_mask])
+	$AnimatedSprite2D.play("IDLE")
+
+func _physics_process(delta: float) -> void:
+	
+	jump_move_disable_time += delta
+	direction = 0.0
+	#var direction := Input.get_axis("move_left", "move_right")
+	if jump_move_disable_time >= WALL_JUMP_DISABLE_TIME or (jump_last_wall_state == jump_last_wall_states.NONE):
+		direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	elif (jump_last_wall_state == jump_last_wall_states.LEFT) and Input.is_action_pressed("move_left"):
+		print("left disabled")
+		direction = Input.get_action_strength("move_right")
+	elif (jump_last_wall_state == jump_last_wall_states.RIGHT) and Input.is_action_pressed("move_right"):
+		print("right_disabled")
+		direction = Input.get_action_strength("move_left")
+		
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		coyote_timer += delta
+	else:
+		coyote_timer = 0
+		
+	# jump.
+	if Input.is_action_just_pressed("jump"):
+		#basic jump
+		if coyote_timer < COYOTE_TIME_LIMIT and current_state != player_states.JUMP: 
+			velocity.y = JUMP_VELOCITY	
+		
+		#walljump before consuming doublejump
+		elif(current_state == player_states.JUMP or player_states.FALL)\
+		and (coyote_timer > COYOTE_TIME_LIMIT)\
+		and (
+			($"RayCast2D-LEFT".is_colliding() and (jump_last_wall_state != jump_last_wall_states.LEFT))\
+		 or ($"RayCast2D-RIGHT".is_colliding() and (jump_last_wall_state != jump_last_wall_states.RIGHT))\
+		):
+			print("WALLJUMP!")
+			print($"RayCast2D-LEFT".is_colliding())
+			print($"RayCast2D-RIGHT".is_colliding())
+			if $"RayCast2D-LEFT".is_colliding() and (jump_last_wall_state != jump_last_wall_states.LEFT):
+				print("jump off LEFT")
+				velocity.y = JUMP_VELOCITY
+				velocity.x += WALL_PUSHBACK
+				jump_last_wall_state = jump_last_wall_states.LEFT
+				jump_move_disable_time = 0
+				jump_count = 0
+			elif $"RayCast2D-RIGHT".is_colliding() and (jump_last_wall_state != jump_last_wall_states.RIGHT):
+				print("jump off RIGHT")
+				velocity.y = JUMP_VELOCITY
+				velocity.x += -WALL_PUSHBACK
+				jump_last_wall_state = jump_last_wall_states.RIGHT
+				jump_move_disable_time = 0
+				jump_count = 0
+			print('WallJumpVelocity:')
+			print(velocity)
+		#Double jump
+		elif (current_state == player_states.JUMP or player_states.FALL) and jump_count < MAX_JUMPS and coyote_timer > COYOTE_TIME_LIMIT:
+			velocity.y = JUMP_VELOCITY	
+			jump_count += 1
+	
+	if is_on_floor(): #reset jump count if on floor
+		jump_count = 0
+		jump_last_wall_state = jump_last_wall_states.NONE
+
+	#colour change controls
+	if Input.is_action_just_pressed("colour_k"):
+		colour_mask = Global.Colour_States.K
+		set_collision_layer_value(2,true)
+		set_collision_mask_value(2,true)
+		set_collision_layer_value(3,false)
+		set_collision_mask_value(3,false)
+		set_collision_layer_value(4,false)
+		set_collision_mask_value(4,false)
+		set_collision_layer_value(5,false)
+		set_collision_mask_value(5,false)
+		
+	if Input.is_action_just_pressed("colour_c"):
+		colour_mask = Global.Colour_States.C
+		set_collision_layer_value(2,false)
+		set_collision_mask_value(2,false)
+		set_collision_layer_value(3,true)
+		set_collision_mask_value(3,true)
+		set_collision_layer_value(4,false)
+		set_collision_mask_value(4,false)
+		set_collision_layer_value(5,false)
+		set_collision_mask_value(5,false)
+		
+	if Input.is_action_just_pressed("colour_y"):
+		colour_mask = Global.Colour_States.Y
+		set_collision_layer_value(2,false)
+		set_collision_mask_value(2,false)
+		set_collision_layer_value(3,false)
+		set_collision_mask_value(3,false)
+		set_collision_layer_value(4,true)
+		set_collision_mask_value(4,true)
+		set_collision_layer_value(5,false)
+		set_collision_mask_value(5,false)
+		
+	if Input.is_action_just_pressed("colour_m"):
+		colour_mask = Global.Colour_States.M
+		set_collision_layer_value(2,false)
+		set_collision_mask_value(2,false)
+		set_collision_layer_value(3,false)
+		set_collision_mask_value(3,false)
+		set_collision_layer_value(4,false)
+		set_collision_mask_value(4,false)
+		set_collision_layer_value(5,true)
+		set_collision_mask_value(5,true)
+
+	# Animation control
+	if not is_on_floor():
+		if velocity.y < 0:
+			current_state = player_states.JUMP
+		else:
+			current_state = player_states.FALL
+	else:
+		if direction:
+			current_state = player_states.RUN
+		else:
+			current_state = player_states.IDLE
+
+	if $AnimatedSprite2D.animation != player_states.keys()[current_state]:
+		$AnimatedSprite2D.play(player_states.keys()[current_state])
+	
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif direction < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		pass
+
+	#$AnimatedSprite2D.material.set_shader_parameter("colour", Global.colourDict[colour_mask])
+	#current_colour. ( Global.colourDict[colour_mask], delta)
+	current_colour = Vector4(
+	lerp(current_colour.x,Global.colourDict[colour_mask].x,5*delta),
+		lerp(current_colour.y,Global.colourDict[colour_mask].y,5*delta),
+		lerp(current_colour.z,Global.colourDict[colour_mask].z,5*delta),
+		1)
+
+	$AnimatedSprite2D.material.set_shader_parameter("colour", current_colour)
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+
+
+
+func die():
+	print("PLAYER DIE")
