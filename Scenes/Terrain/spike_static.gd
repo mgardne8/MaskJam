18c0058c0b43f323fd65992810deedfbbb706bcb
Spmckenn <spmckenn@lakeheadu.ca>
2025-10-13 12:06:55 -0400

    Spike and Charger enemy

---
+++ Scenes/Terrain/spike_static.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.name == "Player":
+		var player : Player = body
+		player.die()	
---
+++ Scripts/state.gd
@@ -0,0 +1,20 @@
+extends Node
+
+class_name State
+
+signal Transitioned
+
+func Enter():
+	pass
+
+func Process_Input(event : InputEvent):
+	pass
+
+func Exit():
+	pass
+
+func Update(_delta: float):
+	pass
+
+func Physics_Update(_delta: float):
+	pass
