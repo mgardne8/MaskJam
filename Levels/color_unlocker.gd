38fe485e7f843a737d833528c65a2b0094786175
Matt Gardner <matt@m-gardner.info>
2025-10-19 14:25:35 -0400

    l1
    l1

--- Levels/color_unlocker.gd
+++ Levels/color_unlocker.gd
@@ -3,9 +3,11 @@ extends Area2D
 
 func _ready() -> void:
 	$"Unlock Text".visible = false
-
-
+	$Left.visible = false
+	$Down.visible = false
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		Global.unlocked_c = true
 		$"Unlock Text".visible = true
+		$Left.visible = true
+		$Down.visible = true
